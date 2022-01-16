cbdb39e542db3b951d5c64e17e0177d4f6c34f27
sohee120 <yuhsohee120@gmail.com>
2022-01-16 23:24:02 +0900

    feat: Coffee Menu

---
+++ 19CafeApp/19CafeApp/MyTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyTableViewCell.swift
+//  19CafeApp
+//
+//  Created by 윤소희 on 2022/01/16.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var imageViewItem: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelPrice: UILabel!
+    
+
+}
