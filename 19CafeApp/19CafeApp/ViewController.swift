69f9d4fa78fac178fd43d2bcbd91e56045b56ad7
sohee120 <yuhsohee120@gmail.com>
2022-01-16 16:34:30 +0900

    UI of MainScreen

---
+++ 19CafeApp/19CafeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  19CafeApp
+//
+//  Created by 윤소희 on 2022/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
